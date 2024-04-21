664371b8d52bdcb6d0ff23d11bb80a16fd5b328c
Jacob Rast <jrast@una.edu>
2024-04-20 19:12:42 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Whoop there it is!'));
+app.get('/', (req, res) => res.send('Here we go again!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
